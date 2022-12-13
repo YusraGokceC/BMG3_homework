b4dd5d6d0f926b56f96182df2dd8fc31808f614c
Zeynep Nisa <zeynepnisaozmen@gmail.com>
2022-12-13 19:14:44 +0300

    simplecalculator eklendi.

---
+++ simplecalculator.py
@@ -0,0 +1,30 @@
+
+# TOPLAMA
+def add(x, y):
+    return x + y
+
+
+# ÇIKARMA
+def subtract(x, y):
+    return x - y
+
+
+# ÇARPMA
+def multiply(x, y):
+    return x * y
+
+
+# BÖLME
+def divide(x, y):
+    return x / y
+
+
+print("IŞLEMLER")
+print("1.Toplama")
+print("2.Çıkarma")
+print("3.Çarpma")
+print("4.Bölme")
+
+while True:
+
+    choice = input("Seçtiğiniz işlemi tuşlayınız(1/2/3/4): ")
